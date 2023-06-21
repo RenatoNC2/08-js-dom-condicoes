36ce43a54eda9db6ed5d6612e22b9852067dd077
RenatoNC2 <132668521+RenatoNC2@users.noreply.github.com>
2023-06-21 01:27:39 +0000

    kjhgk

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function carregar(){ 
     var msg =window.document.getElementById('msg')
-    var img =window.document.getElementById('rt')
+    var img =window.document.getElementById('ft')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML ='agora são ' + hora +' horas.'
+    msg.innerHTML = `agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
         //bom dia
         img.src ='fotoManhã.png'
