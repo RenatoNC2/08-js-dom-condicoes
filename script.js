70a93281e4cabb96046d110a889bb68db6c093c3
RenatoNC2 <132668521+RenatoNC2@users.noreply.github.com>
2023-06-21 00:11:30 +0000

    alteração

---
+++ script.js
@@ -0,0 +1,15 @@
+var msg =window.document.getElementById('msg')
+var msg =window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML ='agora são ${horas} horas.'
+if (horas >= 0 && hora < 12){
+    //bom dia
+    Image.src ='fotoManhã.png'
+}else if (hora >= 12 && hora <18){
+    //boa tarde
+    Image.src ='fotoTade.png'
+}else{
+    //boa noite
+    Image.src ='fotoNoite.png'
+}
