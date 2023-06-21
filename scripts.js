b5c0415a8a22a762ba0ee3faaa112e42fe6e8046
RenatoNC2 <132668521+RenatoNC2@users.noreply.github.com>
2023-06-21 20:58:04 +0000

    jhvk

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 function verificar(){
-    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if(fano.values.length == 0 || fano.values > ano){
+
+    }else{
+        window.alert('sou do caralho')
+    }
 }
